6f4ed781cdd61b06d49d16b286cb4f5c1d301938
John-John Tedro <udoprog@spotify.com>
2015-06-16 06:11:37 +0200

    [spotify] add internal configuration com.spotify.heroic.SpotifyConfiguration

--- heroic-internal-dist/src/main/java/com/spotify/heroic/spotify/SpotifyConfiguration.java
+++ heroic-internal-dist/src/main/java/com/spotify/heroic/spotify/SpotifyConfiguration.java
@@ -1,4 +1,4 @@
-package com.spotify.heroic;
+package com.spotify.heroic.spotify;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
@@ -6,8 +6,10 @@ import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
 
 import com.google.inject.Injector;
+import com.spotify.heroic.HeroicConfigurator;
+import com.spotify.heroic.HeroicInternalLifeCycle;
+import com.spotify.heroic.HeroicService;
 import com.spotify.heroic.HeroicCore.Builder;
-import com.spotify.heroic.HeroicService.Parameters;
 import com.spotify.heroic.statistics.HeroicReporter;
 import com.spotify.heroic.statistics.semantic.SemanticHeroicReporter;
 import com.spotify.metrics.core.MetricId;
@@ -18,27 +20,25 @@ import com.spotify.metrics.jvm.MemoryUsageGaugeSet;
 import com.spotify.metrics.jvm.ThreadStatesMetricSet;
 
 @Slf4j
-public class HeroicInternalService {
-    public static void main(String[] args) throws Exception {
-        HeroicService.main(args, new HeroicService.Configuration() {
+public class SpotifyConfiguration implements HeroicService.Configuration {
+    @Override
+    public void configure(final Builder builder, final HeroicService.Parameters params) {
+        log.info("Setting up spotify-internal configuration");
+
+        final SemanticMetricRegistry registry = new SemanticMetricRegistry();
+        final HeroicReporter reporter = new SemanticHeroicReporter(registry);
+
+        builder.reporter(reporter).configurator(new HeroicConfigurator() {
             @Override
-            public void configure(final Builder builder, final Parameters params) {
-                final SemanticMetricRegistry registry = new SemanticMetricRegistry();
-                final HeroicReporter reporter = new SemanticHeroicReporter(registry);
+            public void setup(final Injector injector) throws Exception {
+                final HeroicInternalLifeCycle lifecycle = injector.getInstance(HeroicInternalLifeCycle.class);
+                final FastForwardReporter ffwd = setupReporter(registry, params.getId());
 
-                builder.reporter(reporter).configurator(new HeroicConfigurator() {
+                lifecycle.registerShutdown("FastForward Reporter", new HeroicInternalLifeCycle.ShutdownHook() {
                     @Override
-                    public void setup(final Injector injector) throws Exception {
-                        final HeroicInternalLifeCycle lifecycle = injector.getInstance(HeroicInternalLifeCycle.class);
-                        final FastForwardReporter ffwd = setupReporter(registry, params.getId());
-
-                        lifecycle.registerShutdown("FastForward Reporter", new HeroicInternalLifeCycle.ShutdownHook() {
-                            @Override
-                            public void onShutdown() {
-                                log.info("Stopping fast forward reporter");
-                                ffwd.stop();
-                            }
-                        });
+                    public void onShutdown() {
+                        log.info("Stopping fast forward reporter");
+                        ffwd.stop();
                     }
                 });
             }
